24609560b73594f4b2824ccb3affaad53eb8822b
spaulsteinberg <sampaulsteinberg@gmail.com>
2020-08-10 23:14:58 -0500

    took out some useless code

--- src/app/components/repo-latest-commits/repo-latest-commits.component.ts
+++ src/app/components/repo-latest-commits/repo-latest-commits.component.ts
@@ -31,7 +31,6 @@ export class RepoLatestCommitsComponent implements OnInit {
           this.http.get(endpoint, this.auth.getHeaders()).subscribe(
             data => {
               for(var d in data){
-                
                 if (data[d] && data[d]["author"]["login"] == "spaulsteinberg"){
                   //extract repo name here -- not explicit in return call
                   let url_extract = (data[d]["commit"]["tree"]["url"].match(/spaulsteinberg(.*?)git/g))[0];
@@ -43,7 +42,7 @@ export class RepoLatestCommitsComponent implements OnInit {
                                   });
                 }
             }
-            this.dataSource= new MatTableDataSource<ICommit>(this.toRender);
+            this.dataSource = new MatTableDataSource<ICommit>(this.toRender);
             this.dataSource.paginator = this.paginator;
           }
           );
@@ -59,51 +58,4 @@ export class RepoLatestCommitsComponent implements OnInit {
   isError:boolean = true;
   displayedColumns: string[] = ['repo', 'date', 'message', 'url'];
   dataSource;
-  /*async getRepos(){
-    try {
-      this.toExport = [];
-      this.data = await this.http.get("https://api.github.com/users/spaulsteinberg/repos", this.auth.getHeaders()).toPromise();
-      const data = this.data;
-      for (var i = 0; i < data.length; i++){
-        this.toExport.push(data[i]["name"]);
-      }
-      this.repoImportedData = (await this.commits.getCommitActivity(this.toExport)).slice(0);
-      console.log(this.repoImportedData);
-      this.isError = false;
-      this.dataToShow();
-      
-    } catch {
-      this.isError = true;
-    }
-  }
-  dataToShow(){
-    console.log("hjere");
-    for (var i = 0; i < this.repoImportedData.length; i++){
-      console.log("here");
-      for (var j = 0; j < this.repoImportedData[i].length; j++){
-        console.log("here");
-        if (j["author"]["login"] == "spaulsteinberg"){
-          console.log("in if");
-          this.toRender.push({url: j["html_url"], 
-                              message: j["commit"]["message"],
-                              date: j["commit"]["author"]["date"],
-                              repoName: this.toExport[i]
-                              });
-         
-        }
-        console.log("I:" + i + " J: " + j)
-      }
-      console.log(i);
-      if (i==(14)){
-        
-      }
-      
-    }
-  }*/
-  
-
-  ngAfterViewInit(){
-    
-  }
-
 }
