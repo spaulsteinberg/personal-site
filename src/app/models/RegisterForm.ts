11ca623516243496944548cb9f3078b6b4355cae
spaulsteinberg <sampaulsteinberg@gmail.com>
2020-08-12 22:13:04 -0500

    made form model and close #25 issue fixed when adding mat style

--- src/app/models/RegisterForm.ts
+++ src/app/models/RegisterForm.ts
@@ -3,4 +3,11 @@ export class RegisterForm {
     lastName:string;
     email:string;
     phoneNumber:string;
+    // phone number isnt required so allow it to be nullable
+    constructor(firstName, lastName, email, phoneNumber?){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber ? phoneNumber : null;
+    }
 }
