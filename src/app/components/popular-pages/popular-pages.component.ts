6b9421ad441c59ed1e6f77daf436d2215c52ead4
spaulsteinberg <sampaulsteinberg@gmail.com>
2020-09-10 10:54:21 -0500

    added percentages to doughnut analytic charts

--- src/app/components/popular-pages/popular-pages.component.ts
+++ src/app/components/popular-pages/popular-pages.component.ts
@@ -29,6 +29,7 @@ export class PopularPagesComponent implements OnInit {
     this._analytics.getMostVisited30days()
         .subscribe(response => {
           let other = 0;
+          let total = 0;
           if (response.status === "OK"){
             response['data'].forEach(element => {
               this.visited.push({
@@ -36,6 +37,7 @@ export class PopularPagesComponent implements OnInit {
                 'views': parseInt(element[2], 10),
                 'avgTimeOnPage': (parseInt(element[3], 10) / 60).toFixed(1)
               });
+              total += parseInt(element[2], 10);
               if (element[2] > 100){
                 this.pathChartLabels.push(element[0]);
                 this.pathChartData.push(element[2]);
@@ -46,6 +48,9 @@ export class PopularPagesComponent implements OnInit {
             });
             this.pathChartLabels.push("Other");
             this.pathChartData.push(other);
+            for (let i = 0; i < this.pathChartData.length; i++){
+              this.pathChartData[i] = ((this.pathChartData[i] / total)*100).toFixed(1);
+            }
           }
           else {
             this.isError = true;
@@ -62,12 +67,14 @@ export class PopularPagesComponent implements OnInit {
     this._analytics.getEventData30days().pipe(
       map(response => {
         let other = 0;
+        let total = 0;
         response['payload'].forEach(element => {
           this.events.push({
             action: element[0],
             totalEvents: element[1],
             uniqueEvents: element[2]
           });
+          total += parseInt(element[1], 10);
           if (element[1] > 30){
             this.eventChartLabels.push(element[0]);
             this.eventChartData.push(element[1]);
@@ -78,6 +85,9 @@ export class PopularPagesComponent implements OnInit {
         });
         this.eventChartLabels.push("Other");
         this.eventChartData.push(other);
+        for (let i = 0; i < this.eventChartData.length; i++){
+          this.eventChartData[i] = ((this.eventChartData[i] / total)*100).toFixed(1);
+        }
       }),
       catchError(this._analytics.getEventDataError)
     ).subscribe({
@@ -106,7 +116,7 @@ export class PopularPagesComponent implements OnInit {
     backgroundColor:[  
       "rgba(255, 99, 132, 0.2)",
       "rgba(255, 159, 64, 0.2)",
-      "rgba(255, 205, 86, 0.2)",
+      "rgba(255, 0, 86, 0.2)",
       "rgba(75, 192, 192, 0.2)",
       "rgba(54, 162, 235, 0.2)",
       "rgba(153, 102, 255, 0.2)",
@@ -139,6 +149,9 @@ export class PopularPagesComponent implements OnInit {
     plugins: {
       datalabels: {
         color: 'turquoise',
+        formatter: function(value){ //pass % in here and have the label display accordingly rounded
+          return (Math.round(value * 10) / 10) + '%';
+        },
         font: {
           weight: 'bold',
           size: 15
