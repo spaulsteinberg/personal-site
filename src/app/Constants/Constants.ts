441cd911e71813f799e10d845ce82c969e6a56aa
spaulsteinberg <sampaulsteinberg@gmail.com>
2021-02-24 09:27:13 -0500

    rem constants

--- src/app/Constants/Constants.ts
+++
@@ -1,6 +0,0 @@
-
-export const GITHUB_API = {
-    TOKEN: "7333e5dd81c805f0abfee28d450a7878a3677670",
-    ENDPOINT: "https://api.github.com/",
-    REPO_ENDPOINT: "https://api.github.com/repos/spaulsteinberg"
-}
