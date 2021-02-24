b0612d77f19e1aca19d1b887ac3fa39c5e1ab38b
spaulsteinberg <sampaulsteinberg@gmail.com>
2021-02-24 09:26:19 -0500

    rem sensitive

--- src/app/Constants/Maps.ts
+++
@@ -1,6 +0,0 @@
-export const MAPS_API = {
-    KEY: "AIzaSyCGaf8FKlEiWkWYIJBvmeTmb_Zx0uX_wjc",
-    BASE_URL: "https://www.google.com/maps/embed/v1/place?",
-    WEATHER_APPID: "a2b8427d127e5d4bcc4f2a4c5f14caf3",
-    WEATHER_BASE: "https://tile.openweathermap.org/map/"
-}
